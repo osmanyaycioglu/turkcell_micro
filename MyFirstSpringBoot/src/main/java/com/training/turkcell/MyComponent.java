3317750d1bf302d6775abbe56e58abd6dbb68717
Osman <osman.yaycioglu@gmail.com>
2020-06-03 14:52:09 +0300

    2

--- MyFirstSpringBoot/src/main/java/com/training/turkcell/MyComponent.java
+++ MyFirstSpringBoot/src/main/java/com/training/turkcell/MyComponent.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Component;
 public class MyComponent {
 
     private String name = "Osman";
+    private String surname;
+    private int    age;
 
     public String getName() {
         return this.name;
@@ -14,5 +16,31 @@ public class MyComponent {
     public void setName(final String nameParam) {
         this.name = nameParam;
     }
+
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+
+    public void setSurname(final String surnameParam) {
+        this.surname = surnameParam;
+    }
+
+
+    public int getAge() {
+        return this.age;
+    }
+
+
+    public void setAge(final int ageParam) {
+        this.age = ageParam;
+    }
+
+    @Override
+    public String toString() {
+        return "MyComponent [name=" + this.name + ", surname=" + this.surname + ", age=" + this.age + "]";
+    }
+
 
 }
--- MyFirstSpringBoot/src/main/java/com/training/turkcell/MyFirstRest.java
+++ MyFirstSpringBoot/src/main/java/com/training/turkcell/MyFirstRest.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,12 +22,12 @@ public class MyFirstRest {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World " + this.myComponent.getName();
+        return "Hello World " + this.myComponent;
     }
 
     @PostMapping("/hello")
     public String hello2() {
-        return "Hello World Post " + this.myComponent.getName();
+        return "Hello World Post " + this.myComponent;
     }
 
     @GetMapping("/name/{abc}")
@@ -33,6 +35,47 @@ public class MyFirstRest {
         this.myComponent.setName(name);
         return "Set name :  " + name;
     }
+
+    @GetMapping("/setall/name/{abc}/surname/{xyz}/age/{asd}")
+    public String setAll(@PathVariable("abc") final String name,
+                         @PathVariable("xyz") final String surname,
+                         @PathVariable("asd") final int age) {
+        this.myComponent.setName(name);
+        this.myComponent.setSurname(surname);
+        this.myComponent.setAge(age);
+        return "Set all :  " + this.myComponent;
+    }
+
+    @GetMapping("/setall2")
+    public String setAll2(@RequestParam("abc") final String name,
+                          @RequestParam("xyz") final String surname,
+                          @RequestParam("asd") final int age) {
+        this.myComponent.setName(name);
+        this.myComponent.setSurname(surname);
+        this.myComponent.setAge(age);
+        return "Set all :  " + this.myComponent;
+    }
+
+    @GetMapping("/setall3/{isim}")
+    public String setAll3(@PathVariable("isim") final String name,
+                          @RequestParam("xyz") final String surname,
+                          @RequestParam("asd") final int age) {
+        this.myComponent.setName(name);
+        this.myComponent.setSurname(surname);
+        this.myComponent.setAge(age);
+        return "Set all :  " + this.myComponent;
+    }
+
+    @GetMapping("/setall4")
+    public String setAll4(@RequestHeader("isim") final String name,
+                          @RequestHeader("xyz") final String surname,
+                          @RequestHeader("asd") final int age) {
+        this.myComponent.setName(name);
+        this.myComponent.setSurname(surname);
+        this.myComponent.setAge(age);
+        return "Set all :  " + this.myComponent;
+    }
+
 
 }
 
